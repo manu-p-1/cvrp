e6a730c6c6fce97dc919549cf80cafdcafe6ecf9
Manu P <28782438+manu-p-1@users.noreply.github.com>
2020-11-11 22:30:21 -0500

    Fixed eval function length checking and import optimization

--- cvrp.py
+++ cvrp.py
@@ -1,16 +1,18 @@
-import json
-import math
 import random
-import util
-from deap import creator, base, tools, algorithms
 
-houses = util.populate_from_file("houses.txt")
+from deap import creator, base, tools
+
+from util import House, Depot, populate_from_file
+
+MAX_CAP = 20
+depot = Depot()
+
+houses = populate_from_file("houses.txt")
 print(houses)
 
 # Specifies the objective (minimize or maximize) and the type of the individual
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMin)
-
 
 # Specifies the representation of the individual and its inclusion into the population
 toolbox = base.Toolbox()
@@ -21,24 +23,18 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 
 def eval_cvrp(individual):
-    # Cost = total distance of n-number of trucks
-    # multiple partitions/trucks?
-    # depot constant?
-
-    max_weight = 20
     current_weight = 0
     routes = {}
     route_counter = 1
-
     distance = 0
 
-    while x in individual:
-            
+    for x in individual:
+
         house = houses[x]
         if route_counter not in routes:
             routes[route_counter] = []
 
-        if current_weight + house.num_packages > 20:
+        if current_weight + house.num_packages > MAX_CAP:
             route_counter += 1
             current_weight = 0
         routes[route_counter].append(house)
@@ -48,10 +44,9 @@ def eval_cvrp(individual):
         for i in range(0, len(route) - 1):
             h1 = route[i]
             h2 = route[i + 1]
-            distance += util.House.distance(h1, h2)
+            distance += House.distance(h1, h2)
 
-        distance += util.House.distance(route[len(route - 1)], House(0, 0, 0, 0))
-
+        distance += House.distance(route[len(route) - 1], depot)
     return distance,
     
 toolbox.register("evaluate", eval_cvrp)
@@ -60,4 +55,3 @@ toolbox.register("mutate", tools.mutGaussian, indpb=0.1, mu=0, sigma=1)
 toolbox.register("select", tools.selTournament, tournsize=5)
 
 pop = toolbox.population(n=10)
-
