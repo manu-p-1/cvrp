b307b78506a24dbd8b37b5f042955916ea9f5d04
Lok-Kwong <46696970+Lok-Kwong@users.noreply.github.com>
2020-11-11 22:18:46 -0500

    Update cvrp.py

--- cvrp.py
+++ cvrp.py
@@ -21,13 +21,43 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 
 def eval_cvrp(individual):
-    # Temporary
-    return individual.x * 2
+    # Cost = total distance of n-number of trucks
+    # multiple partitions/trucks?
+    # depot constant?
 
+    max_weight = 20
+    current_weight = 0
+    routes = {}
+    route_counter = 1
 
+    distance = 0
+
+    while x in individual:
+            
+        house = houses[x]
+        if route_counter not in routes:
+            routes[route_counter] = []
+
+        if current_weight + house.num_packages > 20:
+            route_counter += 1
+            current_weight = 0
+        routes[route_counter].append(house)
+        current_weight += house.num_packages
+    
+    for route_number, route in routes.items():
+        for i in range(0, len(route) - 1):
+            h1 = route[i]
+            h2 = route[i + 1]
+            distance += util.House.distance(h1, h2)
+
+        distance += util.House.distance(route[len(route - 1)], House(0, 0, 0, 0))
+
+    return distance,
+    
 toolbox.register("evaluate", eval_cvrp)
 toolbox.register("mate", tools.cxBlend, alpha=0.5)
 toolbox.register("mutate", tools.mutGaussian, indpb=0.1, mu=0, sigma=1)
 toolbox.register("select", tools.selTournament, tournsize=5)
 
 pop = toolbox.population(n=10)
+
