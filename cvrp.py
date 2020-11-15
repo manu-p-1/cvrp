bf3f282feb59771194b609082203ef4b0a923c83
Manu P <28782438+manu-p-1@users.noreply.github.com>
2020-11-14 20:26:06 -0500

    Added class structure to partition individual to separate routes

--- cvrp.py
+++ cvrp.py
@@ -1,9 +1,47 @@
 import random
+from typing import Dict
+
 from deap import creator, base, tools
+
 from util import House, Depot, populate_from_file
 
-MAX_CAPACITY = 20
-depot = Depot()
+
+class CVRP:
+    MAX_CAPACITY = 20
+
+    def __init__(self):
+        self.depot = Depot()
+
+    def eval_cvrp(self, individual):
+        distance = 0
+        partitioned_routes = self.partition_routes(individual)
+
+        for _, route in partitioned_routes.items():
+            for i in range(len(route) - 1):
+                h1, h2 = route[i], route[i + 1]
+                distance += House.distance(h1, h2)
+            distance += House.distance(route[len(route) - 1], self.depot)
+
+        return distance,
+
+    @classmethod
+    def partition_routes(cls, individual: list) -> Dict:
+        routes = {}
+        current_weight = 0
+        route_counter = 1
+
+        for x in individual:
+            house = houses[x]
+            if route_counter not in routes:
+                routes[route_counter] = []
+
+            if current_weight + house.num_packages > cls.MAX_CAPACITY:
+                route_counter += 1
+                current_weight = 0
+            routes[route_counter].append(house)
+            current_weight += house.num_packages
+        return routes
+
 
 houses = populate_from_file("houses.txt")
 print(houses)
@@ -19,36 +57,12 @@ toolbox.register("indices", random.sample, range(len(houses)), len(houses))
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
+cvrp = CVRP()
 
-def eval_cvrp(individual):
-    routes = {}
-    current_weight = 0
-    route_counter = 1
-    distance = 0
-
-    for x in individual:
-        house = houses[x]
-        if route_counter not in routes:
-            routes[route_counter] = []
-
-        if current_weight + house.num_packages > MAX_CAPACITY:
-            route_counter += 1
-            current_weight = 0
-        routes[route_counter].append(house)
-        current_weight += house.num_packages
-
-    for _, route in routes.items():
-        for i in range(len(route) - 1):
-            h1, h2 = route[i], route[i + 1]
-            distance += House.distance(h1, h2)
-        distance += House.distance(route[len(route) - 1], depot)
-
-    return distance,
-
-
-toolbox.register("evaluate", eval_cvrp)
+toolbox.register("evaluate", cvrp.eval_cvrp)
 toolbox.register("mate", tools.cxBlend, alpha=0.5)
 toolbox.register("mutate", tools.mutGaussian, indpb=0.1, mu=0, sigma=1)
 toolbox.register("select", tools.selTournament, tournsize=5)
 
 pop = toolbox.population(n=10)
+print(pop)
