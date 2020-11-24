3e28aef55d46344959e64a9e6e079c9c63ceb090
Manu P <28782438+manu-p-1@users.noreply.github.com>
2020-11-24 16:30:18 -0500

    Indentation and print generation support

--- driver.py
+++ driver.py
@@ -7,30 +7,34 @@ from util import BuildingEncoder, parse_file
 if __name__ == '__main__':
     pop, sel, ngen, mutpb, cxpb = 200, 5, 50_000, 0.15, 0.75
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", metavar='', type=str, help="The path to the problem set")
-    parser.add_argument("-p", "--pop", metavar='', type=int, help="The population size")
-    parser.add_argument("-s", "--sel", metavar='', type=int, help="The selection size")
-    parser.add_argument("-g", "--ngen", metavar='', type=int, help="The generation size")
-    parser.add_argument("-m", "--mutpb", metavar='', type=float, help="The mutation probability")
-    parser.add_argument("-c", "--cxpb", metavar='', type=float, help="The crossover probability")
+    parser = argparse.ArgumentParser(description="Runs the CVRP with any of the optional arguments")
+    parser.add_argument("-f", "--file", metavar='', type=str, help="the path to the problem set")
+    parser.add_argument("-p", "--pop", metavar='', type=int, help="the population size")
+    parser.add_argument("-s", "--sel", metavar='', type=int, help="the selection size")
+    parser.add_argument("-g", "--ngen", metavar='', type=int, help="the generation size")
+    parser.add_argument("-m", "--mutpb", metavar='', type=float, help="the mutation probability")
+    parser.add_argument("-c", "--cxpb", metavar='', type=float, help="the crossover probability")
+    parser.add_argument("-i", "--indent", metavar='', type=int, help="the indentation amount of the result string")
+    parser.add_argument("-P", "--pgen", action='store_true', help="prints the current generation")
     args = parser.parse_args()
 
-    p_set = parse_file("data/A-n54-k7.ocvrp") if args.file is None else parse_file(args.file)
-    pop = pop if args.pop is None else args.pop
-    sel = sel if args.sel is None else args.sel
-    ngen = ngen if args.ngen is None else args.ngen
-    mutpb = mutpb if args.mutpb is None else args.mutpb
-    cxpb = cxpb if args.cxpb is None else args.cxpb
+    p_set = parse_file(args.file) if args.file else parse_file("data/A-n54-k7.ocvrp")
+    pop = args.pop if args.pop else pop
+    sel = args.sel if args.sel else sel
+    ngen = args.ngen if args.ngen else ngen
+    mutpb = args.mutpb if args.mutpb else mutpb
+    cxpb = args.cxpb if args.cxpb else cxpb
 
     cvrp = CVRP(problem_set=p_set,
                 population_size=pop,
                 selection_size=sel,
                 ngen=ngen,
                 mutpb=mutpb,
-                cxpb=cxpb)
+                cxpb=cxpb,
+                pgen=args.pgen if args.pgen else False)
 
     result = cvrp.run()
+
     print(json.dumps(obj=result,
                      cls=BuildingEncoder,
-                     indent=2))
+                     indent=None if args.indent == 0 else args.indent))
