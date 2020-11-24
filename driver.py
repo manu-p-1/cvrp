9ccf78df14aca394b5444caf244700ea392ec8b1
Manu P <28782438+manu-p-1@users.noreply.github.com>
2020-11-24 15:49:01 -0500

    Refactored how files are inputted as args

--- driver.py
+++ driver.py
@@ -1,41 +1,27 @@
 import argparse
 import json
-import sys
 
 from cvrp import CVRP
-from util import populate_from_file, BuildingEncoder
+from util import BuildingEncoder, parse_file
 
 if __name__ == '__main__':
-
-    pop = 200
-    sel = 5
-    ngen = 50_000
-    mutpb = 0.15
-    cxpb = 0.75
+    pop, sel, ngen, mutpb, cxpb = 200, 5, 50_000, 0.15, 0.75
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--test", action='store_true', help="Use test set")
     parser.add_argument("-f", "--file", metavar='', type=str, help="The path to the problem set")
     parser.add_argument("-p", "--pop", metavar='', type=int, help="The population size")
     parser.add_argument("-s", "--sel", metavar='', type=int, help="The selection size")
-    parser.add_argument("-g", "--gen", metavar='', type=int, help="The generation size")
+    parser.add_argument("-g", "--ngen", metavar='', type=int, help="The generation size")
     parser.add_argument("-m", "--mutpb", metavar='', type=float, help="The mutation probability")
     parser.add_argument("-c", "--cxpb", metavar='', type=float, help="The crossover probability")
     args = parser.parse_args()
 
-    if args.test:
-        p_set = populate_from_file("problems_sets/test_set.txt")
-    else:
-        if args.file:
-            p_set = populate_from_file(sys.argv[1])
-        else:
-            p_set = populate_from_file("problems_sets/A-n54-k7.txt")
-
-    pop = args.pop if args.pop is not None else pop
-    sel = args.sel if args.sel is not None else sel
-    ngen = args.gen if args.gen is not None else ngen
-    mutpb = args.mutpb if args.mutpb is not None else mutpb
-    cxpb = args.cxpb if args.cxpb is not None else cxpb
+    p_set = parse_file("data/A-n54-k7.ocvrp") if args.file is None else parse_file(args.file)
+    pop = pop if args.pop is None else args.pop
+    sel = sel if args.sel is None else args.sel
+    ngen = ngen if args.ngen is None else args.ngen
+    mutpb = mutpb if args.mutpb is None else args.mutpb
+    cxpb = cxpb if args.cxpb is None else args.cxpb
 
     cvrp = CVRP(problem_set=p_set,
                 population_size=pop,
