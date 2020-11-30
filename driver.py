169a96b625aa9e382b79fe55e4f5ca805db92264
Manu P <28782438+manu-p-1@users.noreply.github.com>
2020-11-30 17:35:20 -0500

    Command line and result gathering enhancements

--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import json
 import os
 import sys
@@ -48,7 +49,6 @@ def main():
     ngen = 50_000
     mutpb = 0.15
     cxpb = 0.75
-    sf = False
     cx_algo = algorithms.best_route_xo
     mt_algo = algorithms.inversion_mutation
 
@@ -59,9 +59,6 @@ def main():
     parser.add_argument("-g", "--ngen", metavar='', type=pos_int, help="the generation size")
     parser.add_argument("-m", "--mutpb", metavar='', type=pos_float, help="the mutation probability")
     parser.add_argument("-c", "--cxpb", metavar='', type=pos_float, help="the crossover probability")
-
-    parser.add_argument("-S", "--save", action="store_true", help="saves the fitness values every 1000 generations to"
-                                                                  "a file")
     parser.add_argument("-r", "--run", metavar='', type=int_ge_one, help="the number of times to run the problem")
 
     cx_types = parser.add_mutually_exclusive_group()
@@ -74,6 +71,10 @@ def main():
                         help="the indentation amount of the result string")
     parser.add_argument("-P", "--pgen", action='store_true', help="prints the current generation")
     parser.add_argument("-A", "--agen", action='store_true', help="prints the average fitness every 1000 generations")
+
+    parser.add_argument("-S", "--save", action="store_true", help="saves the results to a file")
+    parser.add_argument("-M", "--plot", action="store_true", help="plot average fitness across generations with "
+                                                                  "matplotlib")
     args = parser.parse_args()
 
     p_set = parse_file(args.file) if args.file else parse_file("data/A-n54-k7.ocvrp")
@@ -81,7 +82,6 @@ def main():
     sel = args.sel if args.sel else sel
     ngen = args.ngen if args.ngen else ngen
     mutpb = args.mutpb if args.mutpb else mutpb
-    sf = args.save if args.save else sf
     cxpb = args.cxpb if args.cxpb else cxpb
 
     runtime = args.run if args.run else 1
@@ -103,14 +103,33 @@ def main():
                 mt_algo=mt_algo,
                 pgen=args.pgen,
                 agen=args.agen,
-                sf=sf)
+                plot=args.plot)
 
-    for i in range(runtime):
+    runs = {"DATA": {}}
+    for i in range(1, runtime + 1):
         result = cvrp.run()
+        runs["DATA"][i] = result
 
-        print(json.dumps(obj=result,
-                         cls=BuildingEncoder,
-                         indent=args.indent))
+        print(f"\n\n============END RUN {i}============\n\n")
+
+    print("...Run Complete")
+    runs['BEST_RUN'] = min(runs['DATA'], key=lambda run: runs['DATA'][run]['best_individual_fitness'])
+    runs['WORST_RUN'] = max(runs['DATA'], key=lambda run: runs['DATA'][run]['best_individual_fitness'])
+    runs["AVG_FITNESS"] = sum(v['best_individual_fitness'] for v in runs['DATA'].values()) / len(runs['DATA'].keys())
+
+    js_res = json.dumps(obj=runs,
+                        cls=BuildingEncoder,
+                        indent=args.indent)
+
+    now = datetime.datetime.now().strftime("%Y%m%d__%I_%M_%S%p")
+    if args.save:
+        with open(f'results/{cvrp.cx_algo}_{cvrp.ngen}__{now}.json', 'w+') as fc:
+            fc.write(js_res)
+
+    if args.plot:
+        for k in runs['DATA'].keys():
+            plt = runs['DATA'][k]['mat_plot']
+            plt.savefig(f'results/{cvrp.cx_algo}_{cvrp.ngen}_MATPLOT__{now}.png')
 
 
 if __name__ == '__main__':
