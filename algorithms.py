30e0088b6b63f9b7a7ef9b5bc1ccf04bc1c80d77
Manu P <28782438+manu-p-1@users.noreply.github.com>
2020-12-10 13:58:21 -0500

    Docstring comments

--- algorithms.py
+++ algorithms.py
@@ -1,3 +1,9 @@
+"""
+https://github.com/manu-p-1/cvrp
+algorithms.py
+
+This module contains a list of functions to perform crossover and mutation operatiosn
+"""
 import math
 import random as r
 from typing import Dict, Union, List
@@ -6,6 +12,24 @@ from util import Building, Individual
 
 
 def best_route_xo(ind1: Individual, ind2: Individual, cvrp) -> Individual:
+    """
+    Given two individuals, does best route crossover proposed by Costa et al.
+
+    Reference:
+    Costa, E., Machado, P., Pereira, B., Tavares, J.,
+    “Crossover and Diversity: A Study about GVR”, In Proceedings of the Analysis and Design of Representations and
+    Operators (ADorO’2003) a bird-of-a-feather workshop at (GECCO-2003), Chicago, Illinois, USA, 12-16 July 2003
+
+    The first and second individuals are partitioned into their GVR form. A random genetic fragment is chosen
+    from a random route from the donor fragment. It is inserted in the recipient individual next to the node that
+    is closest in distance to the first node in the fragment sub-route. Before it is inserted, all duplicates are
+    removed to retain the integrity of the permuation.
+
+    :param ind1: The first Individual
+    :param ind2: The second Individual
+    :param cvrp: An instance of the CVRP class
+    :return: An Individual with the recombinant genes with no fitness evaluated yet.
+    """
     ind1_partitioned = cvrp.partition_routes(ind1)
     ind2_partitioned = cvrp.partition_routes(ind2)
 
@@ -13,11 +37,11 @@ def best_route_xo(ind1: Individual, ind2: Individual, cvrp) -> Individual:
     route_number = r.choice(list(ind1_partitioned.keys()))
     route_from_ind1 = ind1_partitioned[route_number]
 
-    # random route chosen to be replaced in chromosome 2 --> list
+    # Random route chosen to be replaced in chromosome 2 --> list
     section_from_ind1 = route_from_ind1[r.randint(0, len(route_from_ind1) - 1):]
 
     for route_num in ind2_partitioned.keys():
-        # removing duplicates
+        # Removing duplicates before we insert the genes
         for building in ind2_partitioned[route_num][:]:
             if building in section_from_ind1:
                 ind2_partitioned[route_num].remove(building)
@@ -54,8 +78,18 @@ def best_route_xo(ind1: Individual, ind2: Individual, cvrp) -> Individual:
 
 
 class CycleInfo:
+    """
+    A helper class to find cycles given two individuals for cycle crossover
+    """
 
     def __init__(self, father: Individual, mother: Individual):
+        """
+        Creates a new instance of the Cycle info. The cycles are created from father
+        to mother.
+
+        :param father: The first Individual
+        :param mother: The second Individual
+        """
         self._mother = mother
         self._father = father
 
@@ -82,9 +116,18 @@ class CycleInfo:
         return cycle
 
     def get_cycle_info(self):
+        """
+        Helper function which hides the implementation of _get_cycle_info
+        :return: The cycles occurring in both parents as a list
+        """
         return self._get_cycle_info()
 
     def _get_cycle_info(self):
+        """
+        Creates a relationship correspondence map between the mother and father Individuals.
+        We forgo any duplicate or repeating numbers in the cycles
+        :return: The cycles occurring in both parents as a list
+        """
         f_cpy = self._father[:]
         m_cpy = self._mother[:]
         correspondence_map = dict(zip(f_cpy, m_cpy))
@@ -108,6 +151,23 @@ class CycleInfo:
 
 
 def cycle_xo(ind1: Individual, ind2: Individual, cvrp) -> Dict[str, Union[List[None], List[list], List[bool]]]:
+    """
+    Given two individuals, does an optimized version of cycle crossover proposed by Nazif and Lee.
+
+    Reference:
+    Habibeh Nazif and Lai Soon Lee, “Optimised crossover genetic algorithm for capacitated vehicle routing problem”,
+    Applied Mathematical Modeling 36, 2011.
+
+    We decide to modify the proposed crossover by finding as many combinations of cycles within a specified runtime,
+    instead of 2^c (proposed by authors) or 2^5 (recommendation by authors) combinations. The runtime is
+    10 × ln(c + 0.1) + 22 × ln(c) - 19 × ln(c) where c is the cycle length.
+
+    :param ind1: The first Individual
+    :param ind2: The second Individual
+    :param cvrp: An Individual with the recombinant genes with no fitness evaluated yet.
+    :return: An List containing the O-child, E-child, cycles, and raw binaries. The O-child and E-child both have
+    their fitness evaluated in the Individual instance, unlike the other crossover operators.
+    """
     cl = CycleInfo(ind1, ind2).get_cycle_info()
     p_children = []
     cycle_len = len(cl)
@@ -121,8 +181,11 @@ def cycle_xo(ind1: Individual, ind2: Individual, cvrp) -> Dict[str, Union[List[N
         o_child = Individual([None] * len(ind1), None)
         e_child = Individual([None] * len(ind1), None)
 
+        # The binaries represent combination of binaries
         binaries = [bool(r.getrandbits(1)) for _ in cl]
         all_ = len(set(binaries))
+
+        # With binaries of all identical values, we ensure at least one change in the cycle combination
         if all_ == 1:
             ri = r.randint(0, len(binaries) - 1)
             binaries[ri] = not binaries[ri]
@@ -155,7 +218,7 @@ def cycle_xo(ind1: Individual, ind2: Individual, cvrp) -> Dict[str, Union[List[N
             "binaries": binaries
         })
 
-    # Sort based on O-Child fitness
+    # Sort based on O-Child fitness, then grab the corresponding e-child for that o-child as well
     children = min(p_children, key=lambda pc: pc['o-child'].fitness)
     children["e-child"].fitness = cvrp.calc_fitness(children['e-child'])
 
@@ -163,15 +226,29 @@ def cycle_xo(ind1: Individual, ind2: Individual, cvrp) -> Dict[str, Union[List[N
 
 
 def edge_recomb_xo(ind1: Individual, ind2: Individual) -> Individual:
+    """
+    Given two individuals, does an edge recombination proposed by Whitley et al.
+
+    Reference:
+    Whitley, Darrell; Timothy Starkweather; D'Ann Fuquay (1989). "Scheduling problems and traveling salesman:
+    The genetic edge recombination operator". International Conference on Genetic Algorithms. pp. 133–140
+
+    :param ind1: The first Individual
+    :param ind2: The second Individual
+    :return: An Individual with the recombinant genes with no fitness evaluated yet.
+    """
     # May need to enforce size of len(2)
     ind1_adjacent = {}
     ind2_adjacent = {}
     ind_len = len(ind1)
 
+    # Create an adjacency matrix for each node which has a list of its neighbors. This is done
+    # for both individuals
     for i in range(ind_len):
         bldg1 = ind1[i]
         bldg2 = ind2[i]
 
+        # Special cases for finding the correct neighbors for each node
         if i == 0:
             ind1_adjacent[bldg1] = [ind1[i + 1]]
             ind1_adjacent[bldg1].append(ind1[ind_len - 1])
@@ -188,6 +265,7 @@ def edge_recomb_xo(ind1: Individual, ind2: Individual) -> Individual:
             ind2_adjacent[bldg2] = [ind2[i - 1]]
             ind2_adjacent[bldg2].append(ind2[i + 1])
 
+    # Create a union of the neighbors from each individuals' adjacency matrix
     ind_union = {}
     for bldg, neighbors in ind1_adjacent.items():
         ind_union[bldg] = list(set(neighbors) | set(ind2_adjacent[bldg]))
@@ -213,6 +291,15 @@ def edge_recomb_xo(ind1: Individual, ind2: Individual) -> Individual:
 
 
 def order_xo(ind1: Individual, ind2: Individual) -> Individual:
+    """
+    Given two individuals, does an order crossover. Order crossover operates by choosing two indices
+    within the first parent and copying everything within those indices to the child. The rest of the
+    elements go from the second parent to the child. Duplicate values are ignored.
+
+    :param ind1: The first Individual
+    :param ind2: The second Individual
+    :return: An Individual with the recombinant genes with no fitness evaluated yet.
+    """
     bound = len(ind1)
 
     cxp1 = r.randint(0, (bound - 1) // 2)
@@ -250,11 +337,12 @@ def order_xo(ind1: Individual, ind2: Individual) -> Individual:
 
 def inversion_mut(child: Individual) -> Individual:
     """
-    Mutates a child's genes by choosing two random indices between 0 and len(chromosome) - 1.
+    Mutates a child's genes by choosing two random indices between 0 and len(child) - 1.
     From a programming perspective, two indices are chosen: one between 0 and the list midpoint and one
     between the midpoint and the length of the list. Every value between the two chosen indices are mirrored.
     This way, the values mutate while preserving the permutation.
-    :param child: The child as a dictionary object (at this stage only containing a key to the chromosome as a list)
+
+    :param child: The child Individual object
     :return: The mutated child
     """
 
@@ -262,6 +350,7 @@ def inversion_mut(child: Individual) -> Individual:
     idx1 = r.randint(0, mid)
     idx2 = r.randint(mid + 1, len(child) - 1)
 
+    # Swap the values until all values are mirrored
     while idx1 <= idx2:
         _swap(child, idx1, idx2)
         idx1 += 1
@@ -271,6 +360,12 @@ def inversion_mut(child: Individual) -> Individual:
 
 
 def swap_mut(child: Individual) -> Individual:
+    """
+    Mutates a child's genes by choosing two random indices between 0 and len(child) - 1 and swapping
+    their elements.
+    :param child: The child Individual object
+    :return: The mutated child
+    """
     idx1 = r.randint(0, len(child) - 1)
     idx2 = r.randint(0, len(child) - 1)
     _swap(child, idx1, idx2)
@@ -279,10 +374,19 @@ def swap_mut(child: Individual) -> Individual:
 
 
 def gvr_scramble_mut(child, cvrp):
-    ind1_partitioned = cvrp.partition_routes(child)
+    """
+    A scramble mutation with added heuristics. Put's the child into the GVR representation,
+    selects a random route then scrambles every value in that route. This type of mutation
+    tends to be aggressive, however, can be useful for diversity maintenance.
 
-    # choose a random route from chromosome 1
-    # route_number = r.choice(list(ind1_partitioned.keys()))
+    The precondition is that a route must contain at least 4 elements for its mutation to be
+    effective. This is by nature of euclidean distance.
+
+    :param child: The child Individual object
+    :param cvrp: An instance of the CVRP class
+    :return: The mutated child
+    """
+    ind1_partitioned = cvrp.partition_routes(child)
 
     max_list = 1
     cur_max = len(ind1_partitioned[max_list])
@@ -290,9 +394,8 @@ def gvr_scramble_mut(child, cvrp):
         if len(ind1_partitioned[route_num]) > cur_max:
             max_list = route_num
             cur_max = len(ind1_partitioned[route_num])
-    # print(max_list)
-    r.shuffle(ind1_partitioned[max_list])
 
+    r.shuffle(ind1_partitioned[max_list])
     new_genes = cvrp.de_partition_routes(ind1_partitioned)
 
     return Individual(new_genes, cvrp.calc_fitness(new_genes))
